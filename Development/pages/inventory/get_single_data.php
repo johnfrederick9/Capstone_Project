f60a087dc02f66bd274469554c37f3f55716dcc1
xuangb <131904469+xuangb@users.noreply.github.com>
2024-09-25 23:41:28 +0800

    Removed/Updated

--- Development/pages/inventory/get_single_data.php
+++
@@ -1,7 +0,0 @@
-<?php include('../../connection.php');
-$item_id = $_POST['item_id'];
-$sql = "SELECT * FROM tb_inventory WHERE item_id='$item_id' LIMIT 1";
-$query = mysqli_query($con,$sql);
-$row = mysqli_fetch_assoc($query);
-echo json_encode($row);
-?>
