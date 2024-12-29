9bae6c14ec760908da08d470e5db561f9b35a8c5
johnfrederick9 <gelayjohnfrederick0@gmail.com>
2024-12-29 17:31:31 +0800

    update changes

--- Development/footer.php
+++ Development/footer.php
@@ -1,8 +1,4 @@
 <html>
-    <body>
-        <div class='footer'>&copy;<span>2024 DEVELO4. All rights reserved.</span></div>
-    </body>
-    
         <!----===== Calendar JS ===== -->
         <script src="../../assets/js/calendar.js"></script>
 </html>
